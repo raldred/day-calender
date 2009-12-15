558f694919f92caa86b0a7a36b6aa16c35ccb9df
Rob Aldred <raldred@gmail.com>
2009-12-15 18:15:03 +0000

    * added images for first few dates, also realigned images, added copyright messages

--- calendar.rb
+++ calendar.rb
@@ -4,7 +4,7 @@ require 'activesupport'
 require 'haml'
 
 get '/' do
-  year = (params[:year]) ? params[:year] : Time.now.year
+  year = (params[:year]) ? params[:year] : 2010
   @time = Time.mktime(year, "jan", 1, 1, 00, 00)
   haml :index
 end
